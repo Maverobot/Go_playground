8f1974ccf699cdffdf24c03b68a2789c139f97af
Maverobot <quzhengrobot@gmail.com>
2019-04-28 11:48:53 +0200

    useless code

--- gobot/arduino_led_rgb.go
+++ gobot/arduino_led_rgb.go
@@ -17,27 +17,49 @@ import (
 	"gobot.io/x/gobot/platforms/firmata"
 )
 
+func safeIncrease(v byte) (byte, bool) {
+	if v < 255 {
+		return v + 1, false
+	}
+	return 0, true
+}
+
+// RGB contains R, G, B values of a color representation
+type RGB struct {
+	r, g, b byte
+}
+
+// Next increases RGB value by 1, in the order of R <- G <- B
+func (rgb *RGB) Next() {
+	var zeroed bool
+	rgb.b, zeroed = safeIncrease(rgb.b)
+	if zeroed {
+		rgb.g, zeroed = safeIncrease(rgb.g)
+		if zeroed {
+			rgb.r, _ = safeIncrease(rgb.r)
+		}
+	}
+}
+
 func main() {
 	firmataAdaptor := firmata.NewAdaptor(os.Args[1])
 	ledR := gpio.NewLedDriver(firmataAdaptor, "6")
 	ledG := gpio.NewLedDriver(firmataAdaptor, "5")
 	ledB := gpio.NewLedDriver(firmataAdaptor, "3")
 
-	leds := []*gpio.LedDriver{ledR, ledG, ledB}
+	rgb := &RGB{
+		r: 0,
+		g: 0,
+		b: 0,
+	}
 
-	var count int
-	count = 0
 	work := func() {
-		gobot.Every(2*time.Second, func() {
-			for i, led := range leds {
-				if i != count {
-					led.Off()
-				} else {
-					led.On()
-				}
-			}
-			count++
-			count = count % len(leds)
+		gobot.Every(time.Microsecond, func() {
+			fmt.Printf("rgb = [%d, %d, %d]\n", rgb.r, rgb.g, rgb.b)
+			ledR.Brightness(rgb.r)
+			ledG.Brightness(rgb.g)
+			ledB.Brightness(rgb.b)
+			rgb.Next()
 		})
 	}
 
