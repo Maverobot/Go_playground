742bebda4a3d9c56de1f0c28bbfb1ef6eb001f0b
Maverobot <quzhengrobot@gmail.com>
2020-03-02 16:31:14 +0100

    ADD: credentials

--- rest-api-example/main.go
+++ rest-api-example/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"strings"
+	"syscall"
 	"time"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-	api_url := "https://httpbin.org/get"
-	req, err := http.NewRequest(http.MethodGet, api_url, nil)
+	apiUrl := "https://httpbin.org/get"
+	get_info(apiUrl)
+}
+
+func get_info(apiUrl string) string {
+	req, err := http.NewRequest(http.MethodGet, apiUrl, nil)
 	if err != nil {
 		panic(err)
 	}
+
+	// For the example the credentials are not needed
+	username, passwd := credentials()
+	req.SetBasicAuth(username, passwd)
+
 	client := http.Client{Timeout: 1 * time.Second}
 	resp, err := client.Do(req)
 	if err != nil {
@@ -25,4 +40,22 @@ func main() {
 		panic(err)
 	}
 	fmt.Printf("%v", string(body))
+	return string(body)
+}
+
+func credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, err := terminal.ReadPassword(int(syscall.Stdin))
+	fmt.Println()
+	if err != nil {
+		panic(err)
+	}
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
 }
